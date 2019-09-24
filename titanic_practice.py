ecf782665bf84fa561537cdf867857cf4ad057fc
Constantine <blackphoenix343@gmail.com>
2019-09-24 22:28:46 +0300

    Corrections
    Particularly of imputing the Age column values

--- titanic_practice.py
+++ titanic_practice.py
@@ -12,8 +12,9 @@ Legend:
 import pandas as pd
 import matplotlib.pyplot as plt; import seaborn as sns
 import copy
+import numpy as np
+
 data = pd.read_csv('titanic_train.csv')
-data_orig = copy.deepcopy(data)
 
 def to_categorical(data, verbose=0):
     """
@@ -38,22 +39,51 @@ data = data.drop(columns=['Cabin', 'Name', 'Ticket' ])  #not informative in any 
 data = to_categorical(data)
 
 #Basic initial EDA
-correl = data.corr()
 data.Pclass = data.Pclass.map({1:'Upper', 2:'Medium', 3:'Poor'})  #rename Pclasses for EDA since numbers are not intuitive to me
 sns.boxplot(x='Sex', y='Age', data=data,hue='Survived').set_title('Survivors by Age and Sex'); plt.show()
-sns.heatmap(correl, annot=True); plt.tight_layout(); plt.show()
+plt.legend(['Died', 'Survived'])
+plt.show()
 
 #Imputing NaN ages with the mean ages per Class, obtained visually and in table
 sns.boxplot(x='Pclass', y='Age', data=data); plt.show()
 print(data.groupby(by='Pclass').mean())
+conditions = [data['Pclass'] == 'Upper', data['Pclass']=='Medium', data['Pclass'] == 'Poor']
+values = [37, 29, 24]
+data['Age'] = np.where(data['Age'].isnull(),
+                              np.select(conditions, values),
+                              data['Age'])
 
-data.Age = data.apply(lambda row: 25 if row['Pclass'] == 'Poor' else (30 if 
-                      row['Pclass'] == 'Medium' else 38), axis=1)
 
 #Impute couple of missing values with most common Embarked station, won't change much either way
 data.Embarked.fillna('C',inplace=True)
 assert(data.isna().sum().sum() == 0)
- 
+data.rename(columns={'Parch': 'Parents/Children', 'SibSp': 'Siblings/Spouses'},inplace=True) 
+data.Survived = data.Survived.map({1:'Survived', 0:'Died'})
+hue_order = ['Died', 'Survived']
+
+#It plots the ROWS as individual bars, hence cleaner that way
+group0= data.groupby(by=['Sex', 'Survived']).Survived.count().unstack()
+group0.plot(kind='bar', stacked=True).set_title('Survivors by Sex')
+plt.ylabel('# of Passengers')
+plt.show()
+
+print('*'*10)
+group = data.groupby(by=['Pclass', 'Survived']).Pclass.count().unstack()
+group.plot(kind='bar', stacked=True).set_title('Survivors by Economic Status')
+plt.show()
+
+group = data.groupby(by=['Parents/Children', 'Survived'])['Parents/Children'].count().unstack()
+group.plot(kind='bar', stacked=True).set_title('Survivors by # of Parents/Children')
+plt.show()
+
+group = data.groupby(by=['Siblings/Spouses', 'Survived'])['Siblings/Spouses'].count().unstack()
+group.plot(kind='bar', stacked=True).set_title('Survivors by # of Siblings/Spouses')
+plt.show()
+
+group = data.groupby(by=['Embarked', 'Survived']).Embarked.count().unstack()
+group.plot(kind='bar', stacked=True).set_title('Survivors by Embarking Station')
+plt.show()
+
 #NEXT UP: More EDA and Pandas manipulation for answering quantitative Questions!
 
 
